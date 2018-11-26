396256d599d0aad98f1750e7656dae5cfac3f03f
mohammedaliyu136 <mohammedaliyu136@gmail.com>
2018-11-26 11:07:54 +0100

    Delete start server.bat

--- start server.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /C ".\venv\Scripts\activate && python manage.py runserver && cd D:\D1\ && D:"
-start C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/employee/list"
